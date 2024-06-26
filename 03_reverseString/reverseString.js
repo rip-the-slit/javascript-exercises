318174698986cc8631659ac9d0d81b0ff87432f1
kelvingelder24 <kelvingelder24@gmail.com>
2024-06-26 23:05:59 +0000

    Complete exercise 3

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,13 @@
-const reverseString = function() {
-
+const reverseString = function(text) {
+    let reversedText = "";
+    // this loop iterates through the negative indexes of text, slices the character
+    // in each index and directly adds it to the returned string
+    for (let i = -1; i >= -(text.length); i--) {
+            let slicedText = text.substr(i, 1)
+            reversedText += slicedText;
+    }
+    return reversedText;
 };
-
+console.log(reverseString("hiiii"))
 // Do not edit below this line
 module.exports = reverseString;
