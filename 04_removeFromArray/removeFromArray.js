8531048db413a8d8a0cb3e43e20050af37ef322d
kelvingelder24 <kelvingelder24@gmail.com>
2024-06-27 01:37:58 +0000

    Complete exercise 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,15 @@
-const removeFromArray = function() {
+const removeFromArray = function(array) {
+    const elementsToRemove = Array.from(arguments)
+    elementsToRemove.shift();
+    for (element of elementsToRemove) {
+        let index = array.indexOf(element)
+        while (index != -1) {
+            array.splice(index, 1,);
+            index = array.indexOf(element);
+        }
+    }
+    return array;
 };
-
+console.log(removeFromArray([1, 1, 4, 2], 1, 4))
 // Do not edit below this line
 module.exports = removeFromArray;
