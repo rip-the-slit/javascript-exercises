465d8e3211d04062ec1f6384914d82ddf10319c1
kelvingelder24 <kelvingelder24@gmail.com>
2024-07-02 14:18:26 +0000

    Complete exercise 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,15 @@
-const fibonacci = function() {
-
+const fibonacci = function(member) {
+    if (member < 0) return "OOPS"
+    if (member == 0) return 0;
+    let lastMember = 1
+    let lastCurrentMember = 1
+    let currentMember = 1
+    for (let i = 2; i < +member; i++) {
+        currentMember = lastMember + lastCurrentMember
+        lastMember = lastCurrentMember
+        lastCurrentMember = currentMember
+    }
+    return currentMember;
 };
-
 // Do not edit below this line
 module.exports = fibonacci;
