7c6d1ab800fa968d784ceff1ae593bc5a272732d
kelvingelder24 <kelvingelder24@gmail.com>
2024-07-02 03:46:53 +0000

    Complete exercise 9

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,15 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    const originalStr = str
+        .split("")
+        .filter((char) => char != " " && char != "," && char != "." && char != "!")
+        .join("")
+        .toLowerCase()
+    const reversedStr = originalStr
+        .split("")
+        .reverse()
+        .join("")
+    return reversedStr === originalStr
 };
-
+console.log(palindromes('Racecar!'))
 // Do not edit below this line
 module.exports = palindromes;
