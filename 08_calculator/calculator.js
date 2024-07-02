2e03cc57cac2ad39a4b1b986c5959f65e51182a9
kelvingelder24 <kelvingelder24@gmail.com>
2024-07-02 02:47:06 +0000

    Complete exercise 8

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,28 @@
-const add = function() {
-	
+const add = function(...args) {
+	return args.reduce((total, operand) => total += operand)
 };
 
-const subtract = function() {
-	
+const subtract = function(...args) {
+  return args.reduce((total, operand) => total -= operand)
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	return arr.length != 0 ? arr.reduce((total, operand) => total += operand) : 0;
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+	return arr.reduce((total, operand) => total *= operand)
 };
 
-const power = function() {
-	
+const power = function(operand, power = 2) {
+	return operand**power;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+	let result = 1
+  if (num > 1) {
+    for (let i = 1; i <= num; i++) result *= i;}
+  return result
 };
 
 // Do not edit below this line
