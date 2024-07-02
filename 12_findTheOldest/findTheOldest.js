af74da97904dcaece6073b6c96d82f5b552452bb
kelvingelder24 <kelvingelder24@gmail.com>
2024-07-02 16:00:47 +0000

    Complete exercise 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,16 @@
-const findTheOldest = function() {
-
-};
-
+const findTheOldest = function(people) {
+    return people.reduce((oldest, current) => {
+        if (!oldest["yearOfDeath"]) {
+            oldest["yearOfDeath"] = new Date().getFullYear()
+        }
+        if (!current["yearOfDeath"]) {
+            current["yearOfDeath"] = new Date().getFullYear()
+        }
+        if ((oldest.yearOfDeath - oldest.yearOfBirth) < (current.yearOfDeath - current.yearOfBirth)) {
+            oldest = current
+        }
+        return oldest
+    })
+}
 // Do not edit below this line
 module.exports = findTheOldest;
